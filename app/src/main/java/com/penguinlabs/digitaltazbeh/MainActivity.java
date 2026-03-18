5c4979561ea58fc1c79acfe815ba7fc150898ec5
exploremaruf <cyberistmaruf@gmail.com>
2026-03-18 13:10:15 +0600

    Initial Commit

---
+++ app/src/main/java/com/penguinlabs/digitaltazbeh/MainActivity.java
@@ -0,0 +1,64 @@
+package com.penguinlabs.digitaltazbeh;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    AppCompatButton add, sub, reset;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tv = findViewById(R.id.tv);
+        add = findViewById(R.id.add);
+        sub = findViewById(R.id.sub);
+        reset = findViewById(R.id.reset);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = count + 1;
+                tv.setText("" + count);
+
+            }
+        });
+
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = count - 1;
+                tv.setText("" + count);
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = 0;
+                tv.setText("" + count);
+            }
+        });
+
+
+    }
+}
